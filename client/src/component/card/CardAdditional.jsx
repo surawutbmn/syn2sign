b9339cc7ddfc5ebdb259b36f85bdb01d18cb91ad
Chawakorn Jitpathak <jitpathak.c@gmail.com>
2024-08-02 03:54:36 +0700

    exhi

--- client/src/component/card/CardAdditional.jsx
+++ client/src/component/card/CardAdditional.jsx
@@ -12,7 +12,7 @@ const CardAdditional = (props) => {
     <div>
       <CardBox className="row txt-grey">
         <div className="col-4">
-            <ImagePreview src="src/Pages/Exhibition/img/20240614-facebook-profile.png"/>
+            <ImagePreview src="/s2s-logo/20240614-facebook-profile.png"/>
         </div>
         <div className="col-8 position-relative">
             <div className="txt-head2 txt-second text-start mt-4">Youtube playlist • Senior Project Showreel, Academic Year 2023</div>
