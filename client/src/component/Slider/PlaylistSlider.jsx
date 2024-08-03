81b5d003caa1161f397c08867e4797181d0334a4
Surawut Binmamud <binmamud_s@silpakorn.edu>
2024-08-03 18:52:18 +0700

    update slide

--- client/src/component/Slider/PlaylistSlider.jsx
+++ client/src/component/Slider/PlaylistSlider.jsx
@@ -8,10 +8,18 @@ import CardAdditional from "../card/CardAdditional";
 
 export default function PlaylistSlider() {
   return (
-    <div className="swiper-content">
+    <div className="position-relative">
+      <div className="prj-pl-nav">
+        <div className="swiper-button-prev"></div>
+        <div className="swiper-button-next"></div>
+      </div>
       <Swiper
         modules={[Navigation]}
-        // slidesPerView={2}
+        slidesPerView={1.1}
+        navigation={{
+          nextEl: ".prj-pl-nav .swiper-button-next",
+          prevEl: ".prj-pl-nav .swiper-button-prev",
+        }}
         breakpoints={{
           768: { slidesPerView: 1.1 },
           1440: { slidesPerView: 1.1 },
@@ -29,7 +37,6 @@ export default function PlaylistSlider() {
         <SwiperSlide>
           <CardAdditional></CardAdditional>
         </SwiperSlide>
-
       </Swiper>
     </div>
   );
