e0b1b66f1890557251345faed9cb89f5780e3941
Surawut Binmamud <binmamud_s@silpakorn.edu>
2024-08-15 05:08:05 +0700

    Update index.jsx

--- client/src/Pages/Exhibition/index.jsx
+++ client/src/Pages/Exhibition/index.jsx
@@ -54,10 +54,14 @@ function Exhibition() {
       <div className="bg-gd-btr"></div>
       <div className="exh-banner exh-banner-bg">
         <div className="exh-banner-overlay"></div>
-        <img src={`${import.meta.env.VITE_BASE_URL}${syn_ele}`} alt="syn2sign element" className="exh-banner-ele" />
+        <img
+          src={`${import.meta.env.VITE_BASE_URL}${syn_ele}`}
+          alt="syn2sign element"
+          className="exh-banner-ele"
+        />
         <div className="git-link">
           <a
-            href="https://syn2sign-test.netlify.app"
+            href="https://github.com/surawutbmn/syn2sign"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -211,7 +215,11 @@ function Exhibition() {
       <Container className="">
         <div className="feedback-con">
           <div className="paper-plane">
-            <img src={`${import.meta.env.VITE_BASE_URL}${paper_plane}`} alt="paper plane" loading="lazy" />
+            <img
+              src={`${import.meta.env.VITE_BASE_URL}${paper_plane}`}
+              alt="paper plane"
+              loading="lazy"
+            />
           </div>
           <span className="txt-prim feedback-title">
             ส่งมอบพลังและกำลังใจถึงผู้จัดงาน (จากผู้ร่วมงาน)
