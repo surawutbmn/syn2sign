5351dc91230554e0987b922b359aa87131324950
Surawut Binmamud <binmamud_s@silpakorn.edu>
2024-08-14 23:08:49 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/syn2sign/demo/",
+  // base: "/syn2sign/demo/",
   plugins: [react()],
 });
